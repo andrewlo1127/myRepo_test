7aa706fab8a01d98b1bdfb929a0a2fdc9158987b
LAPTOP-5VPSEJN9\loyen <loyenhsiang@gmail.com>
2023-10-16 11:05:39 +0800

    dbconfig

--- dbconfig.php
+++ dbconfig.php
@@ -3,8 +3,8 @@
 連線資料庫用的副程式
 */
 $host = ''; //執行DB Server 的主機
-$user = ''; //登入DB用的DB 帳號
-$pass = ''; //登入DB用的DB 密碼
+$user = '123'; //登入DB用的DB 帳號
+$pass = '123'; //登入DB用的DB 密碼
 $dbName = ''; //使用的資料庫名稱
 /* $db 即為未來執行SQL指令所使用的物件 */
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
